a30394670024620145991298f905c884680ed7ea
dinesh001 <dineshdevkota001@gmail.com>
2019-08-08 13:13:18 +0545

    commented the necessary part in two files

--- Abstraction.py
+++ Abstraction.py
@@ -1,8 +1,5 @@
-# import pyop
-# from Renderer import Renderer
-# badch = [1,5,8,3]
-# c = Renderer(badch)
-# c.render()
+# this is the abstraction layer which converts the data fr the GUI to use
+
 from Renderer import Renderer
 from Predictor import Predictor
 from Colorer import Colorer
@@ -10,31 +7,34 @@ from Colorer import Colorer
 
 class Abstraction:
     def __init__(self,filePath):
-        self.result = []
-        self.data = []
-        self.color = []
-        self.bad = []
-        self.powers = []
-        self.channelName = []
-        self.eegLength = 0
-        self.r = 0
-        self.filePath = filePath
+        self.result = []  #bad channels and result
+        self.data = []      # raw data from edf as a array
+        self.color = []     #the color array to feed to the renderer
+        self.bad = []       #the list of bad channels
+        self.powers = []    #powerrs of the different waves
+        self.channelName = []   # name of different electrodes
+        self.eegLength = 0      #length of the eeg
+        self.r = 0              #the rendering object
+        self.filePath = filePath    #absolute filepath for the edf file from GUI
     
     def __str__(self):
         return (self.filePath.split('\\')[-1])
 
     def get_data(self):
+        # Gets the result from the SVM model
         p = Predictor(self.filePath)
+        # Gets the result using the SVM model
         p.assess_func()
+        #store result
         self.result = p.result_data
-        self.data = p.raw_data
-        self.channelName = p.header
-        self.eegLength = len(p.raw_data)/256
-        self.bad = p.bad
-        self.powers = p.powers
+        self.data = p.raw_data      #store raw data
+        self.channelName = p.header #store electrodes information for the GUI
+        self.eegLength = len(p.raw_data)/256    #gets the length of the eeg in seconds
+        self.bad = p.bad    # Bad channels in the EEG
+        self.powers = p.powers  # power of different waves in the eeg
     
-    def run(self):
-        self.get_data()
+    def run(self): # the main loop
+        self.get_data() 
         self.make_color()
         
     def make_color(self):
@@ -43,8 +43,4 @@ class Abstraction:
         self.r = Renderer(self.bad,self.color)
     
     def render(self):
-        self.r.render()
-
-# if __name__ == "__main__":  
-#     a = Abstraction('00408')
-#     a.render()
+        self.r.render() #start the rendering engine
--- Colorer.py
+++ Colorer.py
@@ -1,41 +1,46 @@
-import pandas as pd
 import numpy as np
 
 class Colorer:
-    def __init__(self):
-    # csv = pd.read_csv('00409.csv', skipinitialspace=True)
+    def __init__(self): # the name of electrodes to shohw in the 3d model and the color files
         self.electrode = ['FP1','FP2','F7','F3','F4','F8','T3','C3','C4','T4','T5','P3','P4','T6','O1','O2']
-        self.out = []
+        self.color = []
 
     def inverse(self,arr):
+        # Inverts the matrix in the raw data for use in this function
         temp = [[arr[j][i] for j in range(len(arr))] for i in range(len(arr[0]))]
         return temp
 
     def coder(self,data):
-        # data = []
-        # for e in self.electrode:
-        #     data.append(csv[e].tolist())
+        #invert the data
         data = self.inverse(data)
+        # store data as dictionary
         dic = dict(zip(self.electrode,data))
         
-        l = len(data[0])//256
+        # total time in second
+        time = len(data[0])//256
+        # to store information about the data
         processed = []
-        for i in range(l):
+        for time_point in range(time):
             elect = []
-            for e in self.electrode:       
-                start = 256*i
+            for e in self.electrode:
+
+                # start the fast fourier transform for each channel for eaach second
+                start = 256*time_point
                 sec  = dic[e][start:start+256]
                 amps = np.abs(np.fft.fft(sec))
                 amps = amps[:128]
+
+                # some estimation and some normalization effort
                 alpha = sum([i for i in amps[8:13]])/0.01
                 beta = sum([i for i in amps[13:30]])/0.12
                 delta = sum([i for i in amps[1:8]])/0.075
+
+                # append them to a list
                 elect.append(alpha)
                 elect.append(beta)
                 elect.append(delta)
             processed.append(elect)
-        # here is the data
-        # np.savetxt('data2.csv',processed,delimiter=',')
-        
-        self.out = processed
-        return self.out
+
+        #  exports the colors
+        self.color = processed
+        return self.color
