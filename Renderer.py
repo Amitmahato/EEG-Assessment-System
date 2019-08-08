167bcadbd8f947f7c8dc37d67215019574e77e53
dinesh001 <dineshdevkota001@gmail.com>
2019-08-08 15:20:56 +0545

    some additional chanfges

--- Renderer.py
+++ Renderer.py
@@ -1,45 +1,45 @@
 import pygame as pygame
-from pygame.locals import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
+from pygame.locals import DOUBLEBUF, OPENGL
+from OpenGL.GL import glPointSize, GL_TRIANGLES, GL_POINTS, glVertex3fv, glColor3fv, glRotatef, glTranslatef, glBegin, glEnable, glEnd, glClear, GL_DEPTH_TEST, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
+from OpenGL.GLU import gluPerspective
 from Objloader import Objloader
 
+
 class Renderer:
-    def __init__(self,badch,color1):
+    def __init__(self, badch, color1):
         # load headcap
         headcap = Objloader()
         self.vertices = headcap.loader('headcap_version2.obj')
-        
+
         # vertices = headcap.vertices
         self.edges = headcap.index
 
         # bad channels list 0-15
         self.bad_channels = badch
 
-        #load head 
+        # load head
         head = Objloader()
         self.vertices2 = head.loader('head.obj')
         self.edges2 = head.index
 
-
         # color data
-        self.color = color1   
+        self.color = color1
 
-    def EEGCap(self,col):
+    def EEGCap(self, col):
         glBegin(GL_TRIANGLES)
         for edge in self.edges:
             for vertex in edge:
-                x=vertex%15
+                x = vertex % 15
                 glColor3fv(col[3*x:3*(x+1)])
-                glVertex3fv(self.vertices[vertex])   
+                glVertex3fv(self.vertices[vertex])
         glEnd()
 
     def Head(self):
         glBegin(GL_TRIANGLES)
         for edge in self.edges2:
             for vertex in edge:
-                glColor3fv([0.2,0.2,0.2])
-                glVertex3fv(self.vertices2[vertex])   
+                glColor3fv([0.2, 0.2, 0.2])
+                glVertex3fv(self.vertices2[vertex])
         glEnd()
 
     def RenderPoints(self):
@@ -47,88 +47,92 @@ class Renderer:
         glBegin(GL_POINTS)
         for i in range(len(self.vertices)):
             if i not in self.bad_channels:
-                glColor3fv([0.1,0.7,0.1])
+                glColor3fv([0.1, 0.7, 0.1])
                 glVertex3fv(self.vertices[i])
         glEnd()
         glPointSize(20)
         glBegin(GL_POINTS)
         for vertex in self.bad_channels:
-            glColor3fv([1.0,0.1,0.2])
+            glColor3fv([1.0, 0.1, 0.2])
             glVertex3fv([i*1.01 for i in self.vertices[vertex]])
         glEnd()
 
     def render(self):
         limit = len(self.color)
         # flags
-        self.showhead = False
+        show_head = False
         drag_lock = False
-        pause = False
-        autorotate = False 
+        is_paused = False
+        autorotate_enable = False
         # counters
         m = 0
         ind = 0
         pygame.init()
-        display = (800,600)
-        pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-        gluPerspective(45 , 4/3 , 0.1 ,50.0)
+        display = (800, 600)
+        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        gluPerspective(45, 4/3, 0.1, 50.0)
         now = []
-        glTranslatef(0.0,0.0,-5)
-        glRotatef(0,0,0,0)
+        glTranslatef(0.0, 0.0, -5)
+        glRotatef(0, 0, 0, 0)
         glEnable(GL_DEPTH_TEST)
-        speed=50
+        speed = 50
         color_len = len(self.color[0])
         diff = []
         for i in range(color_len):
-            diff.append((self.color[(m+1)%limit][i] - self.color[m][i]) / speed)
+            diff.append((self.color[(m+1) % limit]
+                         [i] - self.color[m][i]) / speed)
 
         while True:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                
+
                 if event.type == pygame.KEYDOWN:
                     if pygame.key.get_focused() and pygame.key.get_pressed()[K_SPACE]:
-                        pause = not pause
+                        is_paused = not is_paused
+
                     if pygame.key.get_focused() and pygame.key.get_pressed()[K_GREATER]:
                         if speed > 5:
-                            speed -=5
+                            speed -= 5
                     if pygame.key.get_focused() and pygame.key.get_pressed()[K_LESS]:
-                        speed +=5
+                        speed += 5
                     if pygame.key.get_focused() and pygame.key.get_pressed()[K_n]:
-                        self.showhead = not self.showhead
+                        show_head = not show_head
                     if pygame.key.get_focused() and pygame.key.get_pressed()[K_b]:
-                        autorotate = not autorotate 
+                        autorotate_enable = not autorotate_enable
                 if drag_lock:
                     prev = now
                     now = pygame.mouse.get_pos()
-                    glRotated(1,0,(now[0]-prev[0])/8,0)
+                    glRotatef(1, 0, (now[0]-prev[0])/8, 0)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 4:
-                        glTranslatef(0,0,1.0)
+                        glTranslatef(0, 0, 1.0)
                     if event.button == 5:
-                        glTranslatef(0,0,-1.0)
-                    if event.button == 1:            
+                        glTranslatef(0, 0, -1.0)
+                    if event.button == 1:
                         drag_lock = True
                         now = pygame.mouse.get_pos()
 
                 if event.type == pygame.MOUSEBUTTONUP:
-                    if event.button == 1:            
+                    if event.button == 1:
                         drag_lock = False
-            
-            if autorotate:
+
+            if autorotate_enable:
                 glRotatef(0.4, 0, 0.4, 0)
-            if not pause:
-                ind += 1  
+            if not is_paused:
+                ind += 1
                 if ind % speed == 0 and m < limit - 1:
                     ind = 0
-                    m = (m+1)%limit
+                    m = (m+1) % limit
                     for i in range(color_len):
-                        diff.append((self.color[(m+1)%limit][i] - self.color[m][i]) / speed)
-                col = [self.color[m][i] + (diff[i])*(ind+1) for i in range(color_len)]
+                        diff.append(
+                            (self.color[(m+1) % limit][i] - self.color[m][i]) / speed)
+                col = [self.color[m][i] + (diff[i])*(ind+1)
+                       for i in range(color_len)]
 
-            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-            if self.showhead:
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            if show_head:
                 self.Head()
             self.EEGCap(col)
             self.RenderPoints()
--- main.py
+++ main.py
@@ -12,59 +12,61 @@ class MainApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         self.browsingHistory = []
         tk.Tk.__init__(self, *args, **kwargs)
-        tk.Tk.title(self,"EEG Assessment System")
+        tk.Tk.title(self, "EEG Assessment System")
         width = 750
         height = 450
         x = (self.winfo_screenwidth() // 2) - (width // 2)
         y = (self.winfo_screenheight() // 2) - (height // 2)
         self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-        tk.Tk.resizable(self,0,0)
+        tk.Tk.resizable(self, 0, 0)
 
         self.container = tk.Frame(self)
         self.container.pack(side="top", fill="both", expand=True)
 
-        self.container.grid_rowconfigure(0,weight=1)
-        self.container.grid_columnconfigure(0,weight=1)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-    def showFrame(self,frameclass,*args):
+    def showFrame(self, frameclass, *args):
         self.browsingHistory.append(frameclass)
         if frameclass in self.frames.keys():
             # print("You are in "+frameclass.__name__)
-            #get the instance of frameclass
+            # get the instance of frameclass
             f = self.frames[frameclass]
-            #if frame is of StartPage update the file hierarchy and the raise it to top
+            # if frame is of StartPage update the file hierarchy and the raise it to top
             if frameclass.__name__ == 'StartPage':
                 if self.browsingHistory[-2].__name__ == 'Results':
                     pass
                 else:
                     for file in FILESPATH:
-                        if len(file)>0:
+                        if len(file) > 0:
                             try:
-                                f.insertIntoHierarchy(parent='',filePath=file)
+                                f.insertIntoHierarchy(parent='', filePath=file)
                             except:
-                                messagebox.showwarning('Already Exist','The file you seleceted already exist in the file hierarchy.')
+                                messagebox.showwarning(
+                                    'Already Exist', 'The file you seleceted already exist in the file hierarchy.')
             elif frameclass.__name__ == 'Upload':
                 f.filePath.set('')
-            elif frameclass.__name__ == 'Results':      ##error has to be solved from here
+            elif frameclass.__name__ == 'Results':  # error has to be solved from here
                 self.frames[frameclass].initUI(args)
                 # frameclass.initUI(args)
             f.tkraise()
         else:
-            frame = frameclass(self.container,self,args)
+            frame = frameclass(self.container, self, args)
             self.frames[frameclass] = frame
-            frame.grid(row=0,column=0,sticky='news')
+            frame.grid(row=0, column=0, sticky='news')
             frame.tkraise()
 
-    def center(self,width,height):
+    def center(self, width, height):
         self.update_idletasks()
         x = (self.winfo_screenwidth() // 2) - (width // 2)
         y = (self.winfo_screenheight() // 2) - (height // 2)
         self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
-if __name__ == "__main__":
-    f = open("firstTime.txt",mode="r")
+
+def main():
+    f = open("firstTime.txt", mode="r")
     lines = f.read().splitlines()
     isFirstTime = int(lines[0].split("=")[1])
     hospitalName = lines[1].split("=")[1]
@@ -79,3 +81,7 @@ if __name__ == "__main__":
     else:
         mainapp.showFrame(StartPage)
     mainapp.mainloop()
+
+
+if __name__ == "__main__":
+    main()
